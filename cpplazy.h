31b19a024c3e98f4323418a39162098ee9fa587b
Niklas Bieck <niklasjbieck@gmail.com>
2016-02-21 17:05:35 -0800

    Implement thunk

--- cpplazy.h
+++ cpplazy.h
@@ -11,14 +11,39 @@ namespace cpplazy
     {
     public:
 
-        template <typename... Args>
-        thunk(std::function<T(Args...)> f, Args... args);
+        //A thunk can be constructed from a function returning T, and
+        //taking an arbitrary number of arguments, all of which have to
+        //be provided.
+        //Keep in mind that functions taking pointers might not be suited
+        //because the location pointed to might no longer be valid
+        template <typename F, typename... Args>
+        thunk(F f, Args&&... args)
+            : m_call(std::bind(f, std::forward<Args>(args)...)), m_computed(false)
+        {}
 
-        operator T();
+        operator T()
+        {
+            if (!m_computed)
+                ComputeValue();
 
-        const T& get();
+            return m_value;
+        }
+
+        const T& get()
+        {
+            if (!m_computed)
+                ComputeValue();
+
+            return m_value;
+        }
 
     private:
+
+        void ComputeValue()
+        {
+            m_value = m_call();
+            m_computed = true;
+        }
 
         // This is the function that produces the value
         std::function<T(void)> m_call;
